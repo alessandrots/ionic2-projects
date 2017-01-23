f71e857a24171da5656b3215e29fb00995b73042
Alessandro T. Santos <alessandro.teixeira@gmail.com>
2017-01-23 07:23:35 -0200

    atualizando codigo teste para o map.

--- FriendList/src/pages/mapa/mapa.ts
+++ FriendList/src/pages/mapa/mapa.ts
@@ -1,9 +1,11 @@
-import { Component, ViewChild, Input } from '@angular/core';
-import {NavController, Platform, AlertController} from 'ionic-angular';
+import { Component, ViewChild, Input, ElementRef } from '@angular/core';
+import {NavController, Platform, AlertController } from 'ionic-angular';
 // import { NavController, NavParams } from 'ionic-angular';
 // import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, CameraPosition, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
-// import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
-import { GoogleMap, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
+// import { GoogleMap, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker } from 'ionic-native';
+
+// declare var google;
 
 /*
   http://ionicframework.com/docs/v2/native/google-maps/
@@ -14,7 +16,10 @@ import { GoogleMap, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker 
 })
 export class MapaPage {
 
-  @ViewChild('mapaView') mapView;
+  // @ViewChild('mapaView') mapView;
+  //https://forum.ionicframework.com/t/native-google-maps-black-screen/75598/6
+  //https://www.google.com.br/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=ionic+2+Google+Map+set+shows+blank+screen
+  @ViewChild('mapaView') mapView: ElementRef;
   public map: any;
   @Input() public latitude: any;
   @Input() public longitude: any;
@@ -24,116 +29,131 @@ export class MapaPage {
   constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController) {
   }
 
-  ngOnInit() {
-  // ngAfterViewInit() {	
-	    if (!this.map) {
-	    	console.log('this.mapView.nativeElement = ', this.mapView.nativeElement);
-	      	this.map = new GoogleMap(this.mapView.nativeElement);
-	      	this.map.setDebuggable(true);
-	    }
+  // ngOnInit() {
+  // // ngAfterViewInit() {
+  // 		let latLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
 
-	    GoogleMap.isAvailable().then(() => {
-	      this.map.setZoom(12);
-	      this.map.setMyLocationEnabled(true);
-	      // this.map.clear();
-	      alert('Map is ready!');
-	      // this.montarMarkers();
-	    });
+	 //    let mapOptions = {
+	 //      center: latLng,
+	 //      zoom: 15,
+	 //      mapTypeId: 'MAP_TYPE_NORMAL',//google.maps.MapTypeId.ROADMAP,
+	 //      position: latLng
+	 //    }
 
-	    let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
+	
+	 //    if (!this.map) {
+	 //    	console.log('this.mapView.nativeElement = ', this.mapView.nativeElement);
+	 //      	// this.map = new GoogleMap(this.mapView.nativeElement);
+	 //      	this.map = new GoogleMap(this.mapView.nativeElement, mapOptions);
+	 //      	this.map.setDebuggable(true);
+	 //    }
 
-		// create new marker
-		let markerOptions: GoogleMapsMarkerOptions = {
-			   position: ionic,
-			   title: 'Ionic'
-		};
+	 //    GoogleMap.isAvailable().then(() => {
+	 //      this.map.setZoom(12);
+	 //      this.map.setMyLocationEnabled(true);
+	 //      // this.map.clear();
+	 //      alert('Map is ready!');
+	 //      // this.montarMarkers();
+	 //    });
 
-		this.map.addMarker(markerOptions)
-			   .then((marker: GoogleMapsMarker) => {
-			      marker.showInfoWindow();
-		});
-	    // });
-   }
+	 //    let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
+
+		// // create new marker
+		// let markerOptions: GoogleMapsMarkerOptions = {
+		// 	   position: ionic,
+		// 	   title: 'Ionic'
+		// };
+
+		// this.map.addMarker(markerOptions)
+		// 	   .then((marker: GoogleMapsMarker) => {
+		// 	      marker.showInfoWindow();
+		// });
+	 //    // });
+  //  }
 
 
+//***********************
  //  	// Load map only after view is initialize
 	// ngAfterViewInit() {
 	//  	this.loadMap();
 	// }
 
-	// ngOnInit() {
-	//     this.loadMap();
- //    }
+	ngOnInit() {
+	    this.loadMap();
+    }
 
-	// // make sure to create following structure in your view.html file
-	// // <ion-content>
-	// //  <div #map id="map"></div>
-	// // </ion-content>
+	// make sure to create following structure in your view.html file
+	// <ion-content>
+	//  <div #map id="map"></div>
+	// </ion-content>
 
-	// loadMap() {
-	// 	 // create a new map by passing HTMLElement
-	// 	 let element: HTMLElement = document.getElementById('map');
+	loadMap() {
+		 // create a new map by passing HTMLElement
+		 // let element: HTMLElement = document.getElementById('map');
 
-	// 	 let map = new GoogleMap(element, {
-	//           'backgroundColor': 'white',
-	//           'controls': {
-	//             'compass': true,
-	//             'myLocationButton': true,
-	//             'indoorPicker': true,
-	//             'zoom': true
-	//           },
-	//           'gestures': {
-	//             'scroll': true,
-	//             'tilt': true,
-	//             'rotate': true,
-	//             'zoom': true
-	//           },
-	//           'camera': {
-	//             'latLng': location,
-	//             'tilt': 30,
-	//             'zoom': 15,
-	//             'bearing': 50
-	//           }
- //        });
+		 let location = new GoogleMapsLatLng(43.0741904,-89.3809802);
 
-	// 	 // listen to MAP_READY event
-	// 	 map.one(GoogleMapsEvent.MAP_READY).then(() => alert('Map is ready!'));
-	// 	 // map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
- //   //          console.log('Map is ready!');
- //   //       });
+		 let map = new GoogleMap(this.mapView.nativeElement, {
+	          'backgroundColor': 'white',
+	          'controls': {
+	            'compass': true,
+	            'myLocationButton': true,
+	            'indoorPicker': true,
+	            'zoom': true
+	          },
+	          'gestures': {
+	            'scroll': true,
+	            'tilt': true,
+	            'rotate': true,
+	            'zoom': true
+	          },
+	          'camera': {
+	            'latLng': location,
+	            'tilt': 30,
+	            'zoom': 15,
+	            'bearing': 50
+	          }
+        });
 
- //         GoogleMap.isAvailable().then(() => {
-	//       map.setZoom(12);
-	//       map.setMyLocationEnabled(true);
-	//       map.clear();
-	//       // this.montarMarkers();
+		 console.log('passou 1!');
+		 // listen to MAP_READY event
+		 // map.one(GoogleMapsEvent.MAP_READY).then(() => alert('Map is ready!'));
+		 map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            console.log('Map is ready!');
+         });
 
-	//       // // create LatLng object
-	// 	  let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
+         GoogleMap.isAvailable().then(() => {
+	      map.setZoom(12);
+	      map.setMyLocationEnabled(true);
+	      map.clear();
+	      console.log('passou 2!');
 
-	// 	  // create new marker
-	// 	  let markerOptions: GoogleMapsMarkerOptions = {
-	// 		   position: ionic,
-	// 		   title: 'Ionic'
-	// 	  };
+	      // // create LatLng object
+		  let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
 
-	// 	  map.addMarker(markerOptions)
-	// 		   .then((marker: GoogleMapsMarker) => {
-	// 		      marker.showInfoWindow();
-	// 	  });
-	//     });
+		  // create new marker
+		  let markerOptions: GoogleMapsMarkerOptions = {
+			   position: ionic,
+			   title: 'Ionic'
+		  };
+
+		  map.addMarker(markerOptions)
+			   .then((marker: GoogleMapsMarker) => {
+			      marker.showInfoWindow();
+		  });
+	    });
 
 		 
 
-	// 	 // // create CameraPosition
-	// 	 // let position: CameraPosition = {
-	// 	 //   target: ionic,
-	// 	 //   zoom: 18,
-	// 	 //   tilt: 30
-	// 	 // };
+		 // // create CameraPosition
+		 // let position: CameraPosition = {
+		 //   target: ionic,
+		 //   zoom: 18,
+		 //   tilt: 30
+		 // };
 
-	// 	 // // // move the map's camera to position
-	// 	 // map.moveCamera(position);
-	// }
+		 // // // move the map's camera to position
+		 // map.moveCamera(position);
+	}
 
 }
